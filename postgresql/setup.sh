7ebf699a4fc6bc004510e019ce8745202e9eaa6b
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-12-13 09:36:52 -0300

    [postgresql] -> pgadmin installation

--- postgresql/setup.sh
+++ postgresql/setup.sh
@@ -10,6 +10,15 @@ sudo systemctl start postgresql@12-main
 # -> Stop
 sudo systemctl stop postgresql@12-main
 
+# -> restart
+sudo systemctl restart postgresql@12-main
+
+# -> enable
+sudo systemctl enable postgresql.service
+
+# -> status
+sudo systemctl status postgresql.service
+
 # -> enter postgresql user
 sudo -i -u postgres
 
@@ -55,3 +64,16 @@ sudo -u root psql
 
 # -> verifying connection
 \conninfo
+
+# -> connect to a specifc database
+\connect <db_name>
+
+# -> install pgadmin
+# https://digitalinnovation.one/artigos/instale-o-postgresql-com-pgadmin4-no-ubuntu-2004
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main"> /etc/apt/sources.list.d/pgadmin4.list && apt update '
+
+sudo apt update
+
+sudo apt install pgadmin4
